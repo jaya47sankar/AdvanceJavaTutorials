b3cc4eb944f39e9e8995a1f65247a8114a0f0d11
Jayasankar <Jayasankar@DESKTOP-6AOCS10>
2022-01-14 14:18:54 +0530

    14-01-2022 Mixin,  Added by Jayasankar

---
+++ AdvanceJavaTutorials/src/jss/day4/liskov/mixin/WillNotWork.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jss.day4.liskov.mixin;
+
+import java.util.List;
+
+/**
+ *
+ * @author Jayasankar
+ */
+
+class Boat {
+
+  List<String> cargoes;
+
+  public void addCargo(String c){
+    cargoes.add(c);
+  }
+
+  public void removeCargo(String c){
+    cargoes.remove(c);
+  }
+}
+
+ class Airprt {
+
+  List<String> aircrafts;
+
+  public void land(String a) {
+    aircrafts.add(a);
+  }
+
+  public void depart(String a) {
+    aircrafts.remove(a);
+  }
+}
+
+//class Aircraft_Carrier extends Airport, Ship
+//{
+//
+//}
+
+
+public class WillNotWork {
+    
+}
